355ef3466bc106d67915c65145c5c0374757fe25
san <san@wisebits.com>
2021-04-07 17:53:28 +0300

    Fix file handlers

--- src/Hooks/FileHook.php
+++ src/Hooks/FileHook.php
@@ -14,4 +14,38 @@ class FileHook extends Hook
         'file_get_contents',
         'file_put_contents',
     ];
+
+    protected static $fileHandlers = [];
+
+    public static function fopen($filename, $options)
+    {
+        $result = call_user_func_array(__FUNCTION__, func_get_args());
+        self::$fileHandlers[(int) $result] = $filename;
+
+        return $result;
+    }
+
+    public static function fwrite($handler)
+    {
+        // skip std in/out
+        if ($handler === STDOUT) {
+            return call_user_func_array(__FUNCTION__, func_get_args());
+        }
+
+        $filename = self::$fileHandlers[(int) $handler] ?? '';
+
+        return self::hookFunction(__FUNCTION__, func_get_args(), ['filename' => $filename]);
+    }
+
+    public static function fread($handler)
+    {
+        // skip std in/out
+        if ($handler === STDIN) {
+            return call_user_func_array(__FUNCTION__, func_get_args());
+        }
+
+        $filename = self::$fileHandlers[(int) $handler] ?? '';
+
+        return self::hookFunction(__FUNCTION__, func_get_args(), ['filename' => $filename]);
+    }
 }
