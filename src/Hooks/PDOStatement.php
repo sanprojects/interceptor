b88df74e089594bf12a20d919242fb62eb96cef3
san <san@wisebits.com>
2021-04-09 00:20:44 +0300

    CS fix

--- src/Hooks/PDOStatement.php
+++ src/Hooks/PDOStatement.php
@@ -21,18 +21,67 @@ namespace Sanprojects\Interceptor\Hooks;
 
 class PDOStatement extends \PDOStatement
 {
+    protected array $params = [];
+
     public function bindValue($param, $value, $type = \PDO::PARAM_STR)
     {
+        $this->params[$param] = $value;
+
         return parent::bindValue(...func_get_args());
     }
 
     public function bindParam($column, &$variable, $type = \PDO::PARAM_STR, $length = null, $driverOptions = null)
     {
+        $this->params[$column] = $variable;
+
         return parent::bindParam(...func_get_args());
     }
 
     public function execute($params = null)
     {
-        return parent::execute($params);
+        $this->params = array_merge($params ?? [], $this->params);
+
+        return PDOHook::hookFunction(fn() => parent::execute(...func_get_args()), func_get_args(), [$this->fullQuery()]);
+    }
+
+    public function fullQuery(): string
+    {
+        return $this->interpolateQuery($this->queryString, $this->params);
+    }
+
+    /**
+     * Replaces any parameter placeholders in a query with the value of that
+     * parameter. Useful for debugging. Assumes anonymous parameters from
+     * $params are are in the same order as specified in $query
+     *
+     * @param string $query The sql query with parameter placeholders
+     * @param array $params The array of substitution parameters
+     * @return string The interpolated query
+     */
+    private function interpolateQuery($query, $params): string
+    {
+        $keys = [];
+        $values = [];
+
+        # build a regular expression for each parameter
+        foreach ($params as $key => $value) {
+            if (is_string($key)) {
+                $keys[] = '/'.$key.'/';
+            } else {
+                $keys[] = '/[?]/';
+            }
+
+            if (is_string($value)) {
+                $values[$key] = "'" . $value . "'";
+            } elseif (is_array($value)) {
+                $values[$key] = "'" . implode("','", $value) . "'";
+            } elseif (is_null($value)) {
+                $values[$key] = 'NULL';
+            } else {
+                $values[$key] = $value;
+            }
+        }
+
+        return preg_replace($keys, $values, $query, 1);
     }
 }
