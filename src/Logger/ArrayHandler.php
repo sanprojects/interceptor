33e5fbe3d998f4ea6c3a429f21480983e249ec9c
san <7497442+sanprojects@users.noreply.github.com>
2021-05-03 15:46:07 +0300

    Logger array handler

--- src/Logger/ArrayHandler.php
+++ src/Logger/ArrayHandler.php
@@ -2,19 +2,33 @@
 
 namespace Sanprojects\Interceptor\Logger;
 
+use Monolog\Formatter\LineFormatter;
 use Monolog\Handler\TestHandler;
 
 class ArrayHandler extends TestHandler
 {
+    public function __construct()
+    {
+        parent::__construct();
+        $this->setFormatter(new LineFormatter(null, null, true,  true));
+    }
+
     public function getLogs(): array
     {
         $result = [];
         foreach ($this->getRecords() as $record) {
-            $result[] = [
-                'datetime' => $record['datetime']['date'] ?? '',
-                'context' => $record['context'],
+            $row = [
+                'time' => $record['datetime']->format("Y-m-d H:i:s.u"),
                 'message' => $record['message'],
+                'context' => $record['context'],
             ];
+            if ($record['context']) {
+                $row['context'] = $record['context'];
+            }
+            if ($record['extra']) {
+                $row['extra'] = $record['extra'];
+            }
+            $result[] = $row;
         }
 
         return $result;
