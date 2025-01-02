0a3fd5bd759938d11643f78e35138c633ea0bb33
san <7497442+sanprojects@users.noreply.github.com>
2025-01-02 23:55:41 +0200

    php-cs-fixer

--- src/Hooks/RdKafkaHook.php
+++ src/Hooks/RdKafkaHook.php
@@ -5,8 +5,8 @@ namespace Sanprojects\Interceptor\Hooks;
 class RdKafkaHook extends Hook
 {
     protected const HOOKED_CLASSES = [
-        \RdKafka\Producer::class => \Sanprojects\Interceptor\Hooks\RdKafka\Producer::class,
-        \RdKafka\Consumer::class => \Sanprojects\Interceptor\Hooks\RdKafka\Consumer::class,
-        \RdKafka\KafkaConsumer::class => \Sanprojects\Interceptor\Hooks\RdKafka\KafkaConsumer::class,
+        \RdKafka\Producer::class => RdKafka\Producer::class,
+        \RdKafka\Consumer::class => RdKafka\Consumer::class,
+        \RdKafka\KafkaConsumer::class => RdKafka\KafkaConsumer::class,
     ];
 }
